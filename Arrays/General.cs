7f51c9e5e7b042bfefe708c0df748e10a6aedff9
Asen Markov <repset96@gmail.com>
2020-10-13 15:02:37 +0300

    Name Change

--- Arrays/General.cs
+++ Arrays/General.cs
@@ -6,7 +6,7 @@ namespace Arrays
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! My name is Asen Markov");
         }
     }
 }
